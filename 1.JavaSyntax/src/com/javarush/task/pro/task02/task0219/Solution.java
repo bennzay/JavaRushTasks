ec6fffb72569a03f325d38becc4b5b914b86acc4
Use <clacccs@gmail.com>
2023-01-04 21:23:19 +0300

    add solution task0219sec

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0219/Solution.java
@@ -6,6 +6,9 @@ package com.javarush.task.pro.task02.task0219;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int id = 1;
+        int age = 2;
+        String documents = "doc";
+        String behavior = "bach";
     }
 }
