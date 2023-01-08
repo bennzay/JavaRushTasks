4a103e6dd868b8068ecc76e4160ab09eaab3bebe
Use <clacccs@gmail.com>
2023-01-08 23:43:04 +0300

    add solution task0313

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0314/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task03.task0314;
+
+import java.util.Scanner;
+
+/* 
+Сломанная клавиатура
+*/
+
+public class Solution {
+    public static String secret = "AmIGo";
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String input = console.nextLine();
+        if (input.equalsIgnoreCase(secret)){
+            System.out.println("доступ разрешен");
+        }else {
+            System.out.println("доступ запрещен");
+        }
+
+
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task04.task0405;
+
+/* 
+Незаполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int high = 0;
+        while (high < 10){
+            int length = 0;
+            System.out.println();
+            high++;
+            while (length < 20){
+                System.out.print("Б");
+                length++;
+            }
+        }
+
+    }
+}
