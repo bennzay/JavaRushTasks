f3f4879f04a3bf6ac75c6eae43d95229b25d67d7
Use <clacccs@gmail.com>
2023-01-08 11:52:10 +0300

    add solution task0309-04

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0308/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0308/Solution.java
@@ -8,6 +8,18 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int x = console.nextInt();
+        int y = console.nextInt();
+        if (x > 0 && y > 0)
+            System.out.println(1);
+        else if (x < 0 && y > 0) {
+            System.out.println(2);
+        }
+        else if (x < 0 && y < 0) {
+            System.out.println(3);
+        }
+        else System.out.println(4);
+
     }
 }
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int stolb = 0;
+
+        while (stolb < 5) {
+            int str = 0;
+            while (str < 10) {
+                System.out.print("Q");
+                str++;
+            }
+            System.out.println();
+            stolb++;
+        }
+
+    }
+}
