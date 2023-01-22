0c8fbca0995831971f4936c3df4db2aa7641885e
Use <clacccs@gmail.com>
2023-01-22 15:26:44 +0300

    add solution 0410

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0410/Solution.java
@@ -8,7 +8,20 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+        Scanner console = new Scanner(System.in);
+        int min = Integer.MAX_VALUE;
+        int secondmin = Integer.MAX_VALUE;
 
+
+        while (console.hasNextInt()) {
+            int tmp = console.nextInt();
+            if (tmp < min) {
+                secondmin = min;
+                min = tmp;
+            } else if (tmp < secondmin && tmp != min) {
+                secondmin = tmp;
+            }
+        }
+        System.out.println(secondmin);
     }
 }
