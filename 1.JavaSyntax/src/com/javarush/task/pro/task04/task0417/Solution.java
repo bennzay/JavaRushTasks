2da5b95d6b5739c7e783b841a1b7f9167a3c0ba3
Use <clacccs@gmail.com>
2023-01-22 23:06:52 +0300

    add solution 0417

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0417/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0417/Solution.java
@@ -9,10 +9,10 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-            int speedms = console.nextInt();
-            double speedkmh = speedms * 3.6;
-            speedkmh = Math.round(speedkmh);
-            System.out.println(speedkmh);
+        double speedms = console.nextInt();
+        int speedkmh =(int) Math.round(speedms * 3.6);
+        //speedkmh = intMath.round(speedkmh);
+        System.out.println(speedkmh);
 
 
     }
