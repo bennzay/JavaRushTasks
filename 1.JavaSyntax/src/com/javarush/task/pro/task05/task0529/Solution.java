5e39f67cf90a809ec6b56515fbb9625a7ed83520
Use <clacccs@gmail.com>
2023-01-24 00:16:46 +0300

    add solution 0529

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0529/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.pro.task05.task0529;
+
+import java.util.Arrays;
+
+/* 
+Галаксианские роботанки (1)
+*/
+
+public class Solution {
+    public static String robotank = "☖";
+    public static String empty = "_";
+    public static String hit = "🎯";
+    public static int width = 30;
+    public static int height = 10;
+    public static String[][] field = new String[height][width];
+    public static int[][] bombs = new int[height][width];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < bombs.length; i++) {
+            Arrays.fill(bombs[i], 1, 11, 1);
+        }
+        for (int i = 0; i < field.length; i++) {
+            Arrays.fill(field[i], empty);
+        }
+        for (int i = 0; i < field.length; i++) {
+            int j = (int) (Math.random() * 30);
+            field[i][j] = robotank;
+        }
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] == robotank && bombs[i][j] == 1) {
+                    field[i][j] = hit;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
