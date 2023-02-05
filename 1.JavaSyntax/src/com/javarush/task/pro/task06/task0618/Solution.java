1d49e1f6850d07873e8a02536e93341d814b7f0d
bennzay <95316220+bennzay@users.noreply.github.com>
2023-02-05 22:32:48 +0300

    add solution 0618

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0618/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0618/Solution.java
@@ -5,14 +5,16 @@ package com.javarush.task.pro.task06.task0618;
 */
 
 public class Solution {
-    public static String material= "материальный";
-    public static String immaterial= "нематериальный";
+    public static String material = "материальный";
+    public static String immaterial = "нематериальный";
 
     public static void main(String[] args) {
         System.out.println("Из болота вылез нематериальный монстр...");
 
-        //напишите тут ваш код
+        switchSword(immaterial);
     }
 
-    //напишите тут ваш код
+    public static void switchSword(String type) {
+        System.out.println("Меч переключен в " + type + " режим.");
+    }
 }
